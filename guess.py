2aac0b4e5c84cfbf7d918b32ba15384ffec2ea1e
Eddie Zhang <zhangzian1996@gmail.com>
2018-08-14 17:35:28 +0800

    v1

---
+++ guess.py
@@ -0,0 +1,14 @@
+import random
+r = random.randint(1, 100)
+count = 0
+while True:
+	count = count + 1
+	num = input('请猜数字: ')
+	num = int(num)
+	if num == r:
+		print('你猜中了！')
+		break
+	elif num > r:
+		print('比答案大！')
+	elif num < r:
+		print('比答案小！')
